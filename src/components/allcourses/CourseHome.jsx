04791144551254562391dc564ad9ba3cbd61b94c
Anish Mainali <130769914+anishmendes@users.noreply.github.com>
2024-04-08 16:57:22 +0545

    back img 1 done

--- src/components/allcourses/CourseHome.jsx
+++ src/components/allcourses/CourseHome.jsx
@@ -6,7 +6,7 @@ import OnlineCourses from "./OnlineCourses"
 const CourseHome = () => {
   return (
     <>
-      <Back title='Explore Courses' />
+      <img src="./images/back2" alt="" />
       <CoursesCard />
       <OnlineCourses />
     </>
