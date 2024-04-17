66e335a427d98493619b9c618f52fef526aeffde
Anish Mainali <130769914+anishmendes@users.noreply.github.com>
2024-04-17 16:35:04 +0545

    changes

--- src/components/country/Page1.jsx
+++ src/components/country/Page1.jsx
@@ -15,7 +15,47 @@ const Page1 = () => {
 
         <div>
             <h1>Why study in  <span className='titlec'> USA </span>from Nepal?</h1>
+            <p className='parac'>Studying in the USA from Nepal offers unparalleled 
+            academic and personal growth opportunities, with top-ranked universities providing world-class education. Renowned for innovative teaching methods and cutting-edge research facilities, American institutions offer a vibrant campus life.
+             Access to a global network of professionals enhances career prospects and fosters cultural exchange. The welcoming environment for international students, coupled with scholarship options, makes the USA an attractive destination for Nepalese
+              students seeking to pursue academic aspirations and broaden horizons.</p>
+
         </div>
+
+        <div>
+            <h2>Key Advantages of Pursuing Higher Education in the <span className='titlec'>USA</span> for Nepali Students</h2>
+            <p>For Nepali students, selecting the USA as their study abroad destination presents a distinct opportunity to not only enhance academic knowledge but also foster personal and professional development. Here are compelling reasons why opting for the USA is a strategic choice for Nepali students seeking higher education :</p>
+            <div>
+
+            <p>1)Academic Distinction: The USA is home to  numerous top-tier universities renowned for their academic rigor and pioneering teaching approaches. Enrolling in a US university guarantees access to state-of-the-art knowledge and expertise, laying a strong groundwork for forthcoming achievements.</p>
+             <section>
+                <p>2)Financial Aid and Scholarships: While higher education in the USA can be expensive, many universities offer financial aid, scholarships, and assistantships to international students, including those from Nepal, to help alleviate the financial burden.</p>
+             </section>
+
+             <section>
+                <p>3)Global Recognition: Degrees from US universities are recognized worldwide, which can enhance career prospects and open doors to opportunities in Nepal or abroad.</p>
+             </section>
+
+
+             <section>
+              <p>4)Career Opportunities: Graduating from a US university can enhance employability both in Nepal and internationally, as employers often value the skills, knowledge, and cultural adaptability gained from a US education.</p>
+             </section>
+
+             <section>
+                <p>5)Personal Development: Studying abroad requires independence, adaptability, and resilience, all of which contribute to personal growth and self-confidence. Nepali students return home with a broader worldview and valuable life experiences.</p>
+             </section>
+
+              <section>
+                <p>6)Cultural Exchange: Living and studying in the USA exposes Nepali students to different cultures, languages, and perspectives. This cultural immersion fosters personal growth, cross-cultural understanding, and tolerance.</p>
+              </section>
+
+            </div>
+        </div>
+
+        <img src="/images/mid pic usa " alt="USA Page 2" />
+        
+
+
             
 
     </div>
