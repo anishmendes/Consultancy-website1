a57d76c84061895dfc4a82a9e63c7f4e179ebb6e
Anish Mainali <130769914+anishmendes@users.noreply.github.com>
2024-04-23 11:44:26 +0545

    mid pic done aus

--- src/components/country/Page3.jsx
+++ src/components/country/Page3.jsx
@@ -7,6 +7,21 @@ import Faq from "./Faq"
 import './page.css'; // Import CSS file
 
 const Page3 = () => {
+
+  const universities = [
+    { id: 1. , name: 'University of Melbourne', rank: 1 },
+    { id: 2. , name: 'Monash University', rank: 2 },
+    { id: 3. , name: 'The University of Queensland', rank: 3 },
+    { id: 4. , name: 'University of Sydney', rank: 4 },
+    { id: 5. , name: 'Australian National University', rank: 5 },
+    { id: 6. , name: 'UNSW Sydney', rank: 6 },
+    { id: 7. , name: 'University of Adelaide', rank: 7 },
+    { id: 8., name: 'The University of Western Australia', rank: 8 },
+    { id: 9., name: 'University of Technology Sydney', rank: 9 },
+    { id: 10., name: 'TMacquarie University', rank: 10},
+    
+];
+
   return (
     <div  className="usa-page">
      <h1 className='heading1'><span className='span1'>STUDY IN</span>  AUSTRALIA</h1>
@@ -29,47 +44,84 @@ const Page3 = () => {
  
             <p className='parat'><span className='topic'><span className='span-red'>1)</span>Academic Excellence:</span>  Australia boasts world-renowned universities known for their high academic standards and innovative teaching methods, providing Nepali students with quality education and research opportunities.</p>
              <section>
-                <p className='parat'><span className='topic'><span className='span-red'>2)</span>Diverse Program Offerings:</span>  Canada boasts a wide range of programs and courses across various disciplines, providing Nepali students with diverse academic options to pursue their interests and career goals.</p>
+                <p className='parat'><span className='topic'><span className='span-red'>2)</span>Pathways to Permanent Residency:</span>   Australia offers various immigration pathways for international students, including post-graduate work visas and skilled migration programs, providing opportunities for Nepali students to build their careers and settle permanently in Australia.</p>
              </section>
 
              <section>
-                <p className='parat'><span className='topic'><span className='span-red'>3)</span>Affordable Education:</span> Compared to other study destinations like the USA and UK, Canada offers relatively affordable tuition fees and living expenses for international students, making it financially feasible for Nepali students to pursue their education.</p>
+                <p className='parat'><span className='topic'><span className='span-red'>3)</span>Affordable Education:</span> Compared to other study destinations, Australia offers competitive tuition fees and living costs, making it financially feasible for Nepali students to pursue their education without compromising on qualit</p>
              </section>
               
 
              
 
              <section>
-              <p className='parat'><span className='topic'><span className='span-red'>4)</span>Work Opportunities:</span> Canada provides ample opportunities for international students to work part-time during their studies and gain valuable work experience through internships and co-op programs, enhancing their employability post-graduation.</p>
+              <p className='parat'><span className='topic'><span className='span-red'>4)</span>Abundant Work Opportunities:</span>  Australia provides ample opportunities for international students to work part-time during their studies and gain valuable work experience, enhancing their employability upon graduation.</p>
              </section>
 
              <section>
-                <p className='parat'><span className='topic'><span className='span-red'>5)</span>Pathways to Permanent Residency:</span> Canada offers various immigration pathways for international students, including the Post-Graduation Work Permit Program (PGWPP) and provincial nominee programs (PNPs), providing opportunities for Nepali students to obtain permanent residency and build their careers in Canada.</p>
+                <p className='parat'><span className='topic'><span className='span-red'>5)</span>Diverse Program Options:</span> Australia offers a wide range of programs across various disciplines, allowing Nepali students to explore their interests and tailor their educational experience to their career aspirations.</p>
              </section>
 
 
-          
+              <section>
+                <p className='parat'><span className='topic'><span className='span-red'>6)</span>Global Recognition:</span> Degrees from Australian universities are globally recognized and highly regarded by employers worldwide, enhancing the career prospects and global mobility of Nepali students.</p>
+              </section>
 
               <section>
-                <p className='parat'><span className='topic'><span className='span-red'>6)</span>Quality of Life:</span> Canada is known for its high standard of living, safety, and multicultural environment, offering Nepali students a comfortable and enriching living experience while pursuing their studies.</p>
+                <p className='parat'><span className='topic'><span className='span-red'>7)</span>High Quality of Life:</span> Australia is known for its high standard of living, safety, and multicultural environment, offering Nepali students a comfortable and enriching living experience while studying abroad.</p>
               </section>
+          
+
 
             </div>
         </div>
 
-        <img src="/images/mid pic canada " alt="CANADA Page 2"  className='img2' />
+        <img src="/images/mid pic aus " alt="CANADA Page 2"  className='img2' />
         <div class="buttons">
-    <button>About CANADA</button>
+    <button>About Australia</button>
      <button>Universities</button>
      <button>Courses </button>
      <button>Scholarship</button>
      </div>
-    <div>
-        <h2>Top Universities for Nepalese Students in AUSTRALIA</h2>
-        <span>
-            <p>The following table presents the top 10 universities in the AUSTRALIA, listing each university's name, and it’s AUSTRALIA rank.</p>
-        </span>
-    </div>
+    
+
+     <div className='pushdown'>
+            <h2 className='h2-blue'>Top Universities for Nepalese Students in <span className='span-red'> Australia</span></h2>
+            <span>
+                <p>The following table presents the top universities in the Australia, listing each university's name, and it’s Australia rank.</p>
+            </span>
+        </div>
+        
+        <div className="table-s">
+            <table className='table-og'>
+                <thead className='thead'>
+
+                    <tr>
+                        <th scope='col' className='th-scope'>
+                          S.N
+                        </th>
+                        <th scope="col" className="th-scope">
+                            Universities
+                        </th>
+                        <th scope="col" className="th-scope2">
+                        Australia rank (2023)
+                        </th>
+                    </tr>
+
+                </thead>
+
+                <tbody>
+                    {universities.map((university, index) => (
+                        <tr key={university.id} className="tr1">
+                            <td className="th-scope">{index + 1}</td>
+                            <td className="th-scope">{university.name}</td>
+                            <td className="th-scope2">{university.rank}</td>
+                        </tr>
+                    ))}
+                </tbody>
+
+            </table>
+            </div>
 
 
         
