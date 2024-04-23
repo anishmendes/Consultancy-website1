cd79bd488a5432e5e0f90f7350884bacdeb5e5d1
Anish Mainali <130769914+anishmendes@users.noreply.github.com>
2024-04-23 14:00:12 +0545

    uk page photo upto done

--- src/components/country/Page4.jsx
+++ src/components/country/Page4.jsx
@@ -2,8 +2,67 @@ import React from 'react'
 
 const Page4 = () => {
   return (
-    <div>
-      iam page 4
+    
+       <div  className="usa-page">
+     <h1 className='heading1'><span className='span1'>STUDY IN</span> UK</h1>
+        <h3 className='para1'>"Embark on an educational journey like no other – <span className='span-red'> study in the UK and pave the way to a bright future!"</span></h3>
+        <img src="/images/uk page 1" alt="CANADA Page 1"  className='img1'/>
+
+        <div>
+            <h1 className='headingc'>Why study in  <span className='span-red'>UK</span>from Nepal?</h1>
+
+
+            <p className='parac'>The United Kingdom has emerged as a highly desirable destination for international students, including those from Nepal, seeking top-tier education in a dynamic and inclusive environment. Nepalese students are increasingly drawn to the UK for higher education due to several compelling factors, including academic excellence, a diverse array of program offerings, affordability, and an exceptional quality of life. Studying in the UK from Nepal provides access to world-renowned education institutions, a wide range of academic programs, manageable tuition fees, ample employment opportunities, a superior standard of living, avenues for permanent residency, and exposure to a richly diverse society.</p>
+
+        </div>
+        
+
+        <div>
+            <h2 className='h2-blue'>Key Advantages of Pursuing Higher Education in the <span className='span-red'>UK</span> for Nepali Students</h2>
+            <p className='parat'>For Nepali students, selecting the United Kingdom as their study abroad destination presents a unique opportunity to enrich academic knowledge while nurturing personal and professional growth. Here are compelling reasons why opting for the UK is a strategic decision for Nepali students seeking higher education:</p>
+            <div>
+ 
+            <p className='parat'><span className='topic'><span className='span-red'>1)</span>Academic Excellence:</span>  The UK is home to some of the world's most prestigious universities renowned for their academic excellence and research output, providing Nepali students with access to top-quality education.</p>
+             <section>
+                <p className='parat'><span className='topic'><span className='span-red'>2)</span>Pathways to Permanent Residency:</span>    The UK provides various immigration pathways for international students to transition to permanent residency, offering Nepali students opportunities to build their careers and settle in the UK.</p>
+             </section>
+
+             <section>
+                <p className='parat'><span className='topic'><span className='span-red'>3)</span>Diverse Program Options:</span>  UK universities offer a diverse range of academic programs across various disciplines, allowing Nepali students to pursue their interests and career aspirations in fields ranging from arts and humanities to science and engineering</p>
+             </section>
+              
+
+             
+
+             <section>
+              <p className='parat'><span className='topic'><span className='span-red'>4)</span>Career Opportunities:</span>  The UK offers abundant opportunities for internships, part-time work, and networking, enabling Nepali students to gain valuable work experience and enhance their employability while studying or after graduation.</p>
+             </section>
+
+             <section>
+                <p className='parat'><span className='topic'><span className='span-red'>5)</span> Quality of Life:</span>  The UK offers a high standard of living with access to excellent healthcare, public transport, cultural amenities, and recreational activities, ensuring a comfortable and fulfilling experience for Nepali students.</p>
+             </section>
+
+
+              <section>
+                <p className='parat'><span className='topic'><span className='span-red'>6)</span>Global Recognized Qualifications:</span> Degrees from UK universities are widely recognized and respected worldwide, enhancing the career prospects and global mobility of Nepali students.</p>
+              </section>
+
+              <section>
+                <p className='parat'><span className='topic'><span className='span-red'>7)</span>Language Advantage:</span> Studying in an English-speaking country like the UK improves Nepali students' English language proficiency, a valuable skill for their academic and professional pursuits.</p>
+              </section>
+          
+
+
+            </div>
+        </div>
+
+        <img src="/images/mid pic uk " alt="CANADA Page 2"  className='img2' />
+        <div class="buttons">
+    <button>About UK</button>
+     <button>Universities</button>
+     <button>Courses </button>
+     <button>Scholarship</button>
+     </div>
     </div>
   )
 }
