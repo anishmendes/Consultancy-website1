690ac6823f36dc8467cdb49c92eabb8de402ca65
anishmendes <anishmainali31@gmail.com>
2024-05-10 15:38:27 +0545

    journel section updated

--- src/components/journel/JournelCard.jsx
+++ src/components/journel/JournelCard.jsx
@@ -1,17 +1,29 @@
 import React from "react"
 import {journel} from "../../dummydata"
-
+import { Link } from "react-router-dom"
 // import { motion } from 'framer-motion';
 
 const JournelCard = () => {
+    const routeTo = () => {
+        window.scrollTo({
+          top:0,
+      
+        })
+      }
     return (
         <>
         
       
         
         
-        {journel.map((val) => (
-            <div  className="items shadow">
+        {journel.map((val,index) => ( 
+             
+                <Link onClick={routeTo} to={`/country/page${index + 1}`}>
+               
+                    
+            <div 
+              key={index}
+            className="items shadow">
                 <div  className="img">
                     <img  src={val.cover} alt="" />
                 </div>
@@ -35,6 +47,7 @@ const JournelCard = () => {
                     <p>{val.desc}</p>
                 </div>
             </div>
+            </Link>
         ))}
         
         
