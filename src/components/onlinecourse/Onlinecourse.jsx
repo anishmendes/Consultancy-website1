1bfc9cbd64b75de8cb5fa685130c4c4c638f9057
anishmendes <anishmainali31@gmail.com>
2024-05-14 14:56:42 +0545

    dfbndn

---
+++ src/components/onlinecourse/Onlinecourse.jsx
@@ -0,0 +1,13 @@
+import './onlinecourse.css'
+
+import React from 'react'
+
+const Onlinecourse = () => {
+  return (
+    <div>
+      <h1>This is online course section</h1>
+    </div>
+  )
+}
+
+export default Onlinecourse
