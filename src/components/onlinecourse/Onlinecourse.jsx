f08580046317745f1bb4dad3d51d4328acde1168
anishmendes <anishmainali31@gmail.com>
2024-05-14 17:39:13 +0545

    ....

--- src/components/onlinecourse/Onlinecourse.jsx
+++ src/components/onlinecourse/Onlinecourse.jsx
@@ -9,6 +9,18 @@ const Onlinecourse = () => {
 
        <div className='onlineimg'>
        <img src="/onlinecourse.png" alt="onlineimg1"   className='onlineimg1'/>
+
+         <div className='onlinein'> 
+         <div>
+          <h1>  Learn Anywhere, Anytime, at Your Own Pace.</h1>
+         </div>
+         <div>
+          <h1>  Discover the freedom of learning on your own terms with our diverse range of online courses</h1>
+         </div>
+         <div>
+          <button>Find a course</button>
+         </div>
+         </div>
        </div>
 
     </div>
