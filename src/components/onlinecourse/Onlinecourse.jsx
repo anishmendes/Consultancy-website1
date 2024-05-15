fe330a2d8ec8bcab6b0eb7ec365f1e133b1abeae
anishmendes <anishmainali31@gmail.com>
2024-05-15 12:13:10 +0545

    1st component done

--- src/components/onlinecourse/Onlinecourse.jsx
+++ src/components/onlinecourse/Onlinecourse.jsx
@@ -11,19 +11,19 @@ const Onlinecourse = () => {
        <img src="/onlinecourse.png" alt="onlineimg1"   className='onlineimg1'/>
 
          <div className='onlinein'> 
-         <div >
-          <h1>  Learn Anywhere, Anytime, at Your Own Pace.</h1>
-         </div>
-         <div>
-          <h1>  Discover the freedom of learning on your own terms with our diverse range of online courses</h1>
-         </div>
-         <div>
-          <button>Find a course</button>
-         </div>
+         
+         
+          <h1 className='onlineh1'>  Learn Anywhere, Anytime, at Your Own Pace.</h1>
+       
+          <h2 className='onlineh2'>  Discover the freedom of learning on your own terms with our diverse range of online courses</h2>
+          <button className='onlineb'>Find a course</button>
+         
          </div>
        </div>
-
     </div>
+   
+    
+
   )
 }
 
