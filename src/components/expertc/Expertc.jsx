863453f396ea1666dcd57668ec8c03fd3e58e51e
anishmendes <anishmainali31@gmail.com>
2024-05-17 16:53:42 +0545

    dfdfd

--- src/components/expertc/Expertc.jsx
+++ src/components/expertc/Expertc.jsx
@@ -20,9 +20,29 @@ const Expertc = () => {
             <div className='prohead'>
               <h2>Professional and dedicated consulting solutions</h2>
             </div>
+
+            
           
             
 
+          </div>
+
+          <div className='service'>
+            <div className='servicehead'>
+              <h3>Dedicated to Your Global Journey: Professional Consultancy Services for Seamless Relocation Abroad.</h3>
+            </div>
+
+            <div className='service1'>
+           <div className='leftsides'>
+            <img src="./images/lecture1.png" alt="lecture1" />
+           </div>
+           <div className='rightsides'> 
+           <h3>Senior Web developer</h3>
+           <span>Shreya Dhakal</span>
+           <p>Shreya Dhakal is a highly skilled senior web developer with a passion for crafting elegant and efficient web solutions. With extensive experience in both front-end and back-end development, Shreya excels in turning complex concepts into user-friendly interfaces. Their expertise in HTML, CSS, JavaScript, and various frameworks enables them to create dynamic and visually stunning websites. Known for their strong problem-solving abilities and collaborative approach, Shreya is a valuable asset to any development team, driving projects to success with precision and innovation.</p>
+           </div>
+         </div>
+        
           </div>
     </div>
   )
