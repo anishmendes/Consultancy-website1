ece37f4c3ed3c6d770005ed617ddd242dcb463fa
anishmendes <anishmainali31@gmail.com>
2024-05-17 16:19:05 +0545

    expert

--- src/components/expertc/Expertc.jsx
+++ src/components/expertc/Expertc.jsx
@@ -4,7 +4,16 @@ import './expertc.css'
 const Expertc = () => {
   return (
     <div className="expertc">
-      <h1>This is the expert concling section</h1>
+     <div className='itimg'>
+       <img src="./images/itfree.jpg" alt="itimg1"   className='itimg1'/>
+       </div>
+       <div className='itin'> 
+         
+         
+          <h1 className='ith1'> Navigate Your Path with Expert Guidance: Consultancy's Finest Counseling Services.</h1>
+       
+          <h2 className='ith2'> Experience personalized guidance and expert advice tailored to your needs with our consultancy's premier counseling services.</h2>
+          </div>
     </div>
   )
 }
