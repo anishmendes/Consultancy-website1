bda705a8377f8e40c098fb2690f28e1ab02ee891
anishmendes <anishmainali31@gmail.com>
2024-05-25 17:22:34 +0545

    contact us label

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -7,6 +7,9 @@ const Contact = () => {
   const map = 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d904726.6131739549!2d85.24565535!3d27.65273865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2snp!4v1652535615693!5m2!1sen!2snp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" '
   return (
     <>
+     <div className="journelheading">
+      <h1>CONTACT US</h1>
+      </div>
       {/* <Back title='Contact us' /> */}
       <motion.section
         initial={{ opacity: 0, scale: 0.3 }}
